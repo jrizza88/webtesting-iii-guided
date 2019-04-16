2fa9138b92bfbf35522db1081578b2d60b8f40b8
jrizza88 <jetorres88@gmail.com>
2019-04-15 22:09:08 -0400

    made initial test

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and save to reload... if you dare, because the snappening is upon us!
+            You press 'u' to update your changes if that is what you want!
           </p>
           <a
             className="App-link"
